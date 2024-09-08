0670f75f728c4720cf1e53c64dbd165bf01a1fd9
GordinVit <gordin.vit@gmail.com>
2024-09-08 22:37:01 +0300

    PP_3_1_1

---
+++ src/main/java/com/test_3_1_1/Spring_boot/controller/UserController.java
@@ -0,0 +1,60 @@
+package com.test_3_1_1.Spring_boot.controller;
+
+import com.test_3_1_1.Spring_boot.model.User;
+import com.test_3_1_1.Spring_boot.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String showAllUsers(Model model) {
+        List<User> allUsers = userService.findAll();
+        model.addAttribute("users", allUsers);
+        return "users";
+    }
+
+    @GetMapping("/updateUser")
+    public String showUpdateForm(@RequestParam("userId") Long userId, Model model) {
+        User user = userService.findById(userId);
+        model.addAttribute("user", user);
+        return "editUser";
+    }
+
+    @PostMapping("/updateUser")
+    public String updateUser(User user) {
+        userService.saveUser(user);
+        return "redirect:/";
+    }
+
+    @GetMapping("/addUser")
+    public String showAddUserForm(Model model) {
+        model.addAttribute("user", new User());
+        return "addUser";
+    }
+
+    @PostMapping("/addUser")
+    public String addUser(@ModelAttribute("user") User user) {
+        userService.saveUser(user);
+        return "redirect:/";
+    }
+
+    @PostMapping("/deleteUser")
+    public String deleteUser(@RequestParam("userId") Long userId) {
+        userService.deleteById(userId);
+        return "redirect:/";
+    }
+}
+
---
+++ src/main/java/com/test_3_1_1/Spring_boot/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.test_3_1_1.Spring_boot.repository;
+
+import com.test_3_1_1.Spring_boot.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+}
